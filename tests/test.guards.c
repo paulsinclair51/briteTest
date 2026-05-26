573c635307d8aa9ca45435f0683c8ea11c2c494d
Paul Sinclair <16927624+paulsinclair51@users.noreply.github.com>
2026-05-26 15:49:42 -0700

    Initialize test file for guard related functions
    Add documentation and initial tests for guard functions.

--- tests/test.guards.c
+++ tests/test.guards.c
@@ -1,1 +1,36 @@
-tbd
+/**
+ * @file test_guards.c
+ 
+ * @brief Test guard related functions and macros declared in
+          /paulsinclair51/litetests.h (e.g., TEST, RUN).
+ *
+ * @copyright Copyright (c) 2026 paulsinclair51
+ * SPDX-License-Identifier: MIT
+ * For license details, see @ref ../LICENSE "LICENSE" in the
+ * paulsinclair51/lubtype repository root.
+ */
+
+#undef TEST_ORCHESTRATOR
+#include "litetest.h"
+
+// Test litetest guard related functions.
+
+TEST(!strcmp("dummy" == "dummy");
+
+if (inject)
+{ ++test_result.injected_fail;
+TEST(0 && "Inject assertion fail 1");
+}
+
+TEST(!strcmp("dummy" == "dummy");
+
+
+if (inject)
+{	++test_result.injected_fail;
+TEST(0 && "Inject assertion fail 2");
+}
+
+TEST(!strcmp("dummy" == "dummy")
+
+return test_result;
+}
