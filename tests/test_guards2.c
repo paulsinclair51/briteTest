611ef82da48f0babe8eccccda9ed6d4592966c7e
Paul Sinclair <16927624+paulsinclair51@users.noreply.github.com>
2026-05-28 09:43:19 -0700

    Add test cases for LiteTest guard functions
    Implement tests for guard related functions in LiteTest.

---
+++ tests/test_guards2.c
@@ -0,0 +1,29 @@
+/**
+ * @file paulsinclair51/LiteTest/test/test_guards2.c
+ 
+ * @brief Test guard related functions and macros declared in
+          /paulsinclair51/LiteTest/include/litetest.h (e.g., TEST, TESTS).
+ *
+ * @copyright Copyright (c) 2026 paulsinclair51
+ * SPDX-License-Identifier: MIT
+ * For license details, see @ref ../LICENSE "LICENSE" in the
+ * paulsinclair51/LiteTest GitHub repository.
+ */
+
+#define TEST_MODULE guards2
+#include "litetest.h"
+
+// Test LiteTest guard related functions.
+
+DECLARE_FUNC(guards2)
+{ 
+  INIT_TEST;
+ 
+  TEST(!strcmp("dummy" == "dummy");
+  TEST_FAIL;
+  TEST(!strcmp("dummy" == "dummy");
+  TEST_FAIL;
+  TEST(!strcmp("dummy" == "dummy");
+
+  RETURN_RESULT;
+}
