3562593cb56de6d33a3820d4cdb2c6744b2acd36
Paul Sinclair <paulsinclair@verizon.net>
2026-05-28 10:12:03 -0700

    Implement test suite for LiteTest functions
    Add tests for guard related functions and macros in LiteTest.

--- tests/test_litetest.c
+++ tests/test_litetest.c
@@ -1,1 +1,30 @@
+/**
+ * @file paulsinclair51/LiteTest/tests/test_litetest.c
+ 
+ * @brief Test guard related functions and macros declared in
+          /paulsinclair51/LiteTest/include/litetest.h (e.g., TEST, TESTS).
+ *
+ * @copyright Copyright (c) 2026 paulsinclair51
+ * SPDX-License-Identifier: MIT
+ * For license details, see @ref ../LICENSE "LICENSE" in the
+ * paulsinclair51/LiteTest GitHub repository.
+ */
 
+#define TEST_ORCHESTRATOR(test_litetest)
+#include "litetest.h"
+
+// Test LiteTest.
+
+DECLARE_MAIN
+{ 
+  PARSE_ARGS;
+  
+  OPEN_REPORT("LiteTest");
+ 
+  WRITE_RESULTS(test_orchestrator), "Orchestrator");
+  WRITE_RESULTS(TESTS2(test_guards1, test_guards2), "Guards 1 and 2";
+
+  CLOSE_REPORT;
+
+  RETURN_STATUS;
+}
