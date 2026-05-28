901ac046bf87eabc316db0c720aea336ee8033e1
Paul Sinclair <16927624+paulsinclair51@users.noreply.github.com>
2026-05-28 09:18:26 -0700

    Fix file paths and copyright in test_guards.c
    Updated file paths and copyright information in comments.

--- tests/test.guards.c
+++ tests/test.guards.c
@@ -2,12 +2,12 @@
  * @file test_guards.c
  
  * @brief Test guard related functions and macros declared in
-          /paulsinclair51/include/litetest.h (e.g., TEST, TESTS).
+          /paulsinclair51/LiteTest/include/litetest.h (e.g., TEST, TESTS).
  *
  * @copyright Copyright (c) 2026 paulsinclair51
  * SPDX-License-Identifier: MIT
  * For license details, see @ref ../LICENSE "LICENSE" in the
- * paulsinclair51/lubtype repository root.
+ * paulsinclair51/LiteTest GitHub repository.
  */
 
 #undef TEST_ORCHESTRATOR
@@ -17,6 +17,8 @@
 
 DECLARE_FUNC(guards)
 { 
+  INIT_TEST
+ 
   TEST(!strcmp("dummy" == "dummy")
   TEST_FAIL
   TEST(!strcmp("dummy" == "dummy")
