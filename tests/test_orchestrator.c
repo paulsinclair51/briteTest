d667a98aece8ef587f1d5fc4f97476fed8fa2a2f
Paul Sinclair <paulsinclair@verizon.net>
2026-05-28 14:06:43 -0700

    Refactor test functions to use LiteTest macros

--- tests/test_orchestrator.c
+++ tests/test_orchestrator.c
@@ -1,8 +1,8 @@
 /**
  * @file /paulsinclair51/LiteTest/tests/test_orchestrator.c
- 
+ *
  * @brief Test orchestrtor related functions and macros declared in
-          /paulsinclair51/LiteTest/include/litetests.h (e.g., TESTS, TEST2).
+ *        /paulsinclair51/LiteTest/include/litetests.h (e.g., TESTS, TEST2).
  *
  * @copyright Copyright (c) 2026 paulsinclair51
  * SPDX-License-Identifier: MIT
@@ -13,17 +13,15 @@
 #define TEST_MODULE test_orchestrator
 #include "litetest.h"
 
-// Test LiteTest guard related functions.
-
-DECLARE_FUNC(orchestrator)
+DECLARE_FUNC(test_orchestrator)
 { 
-  INIT_TEST;
+  LT_INIT_TEST;
  
-  TEST(!strcmp("dummy" == "dummy");
-  TEST_FAIL;
-  TEST(!strcmp("dummy" == "dummy");
-  TEST_FAULT;
-  TEST(!strcmp("dummy" == "dummy");
+  LT_ASSERT(!strcmp("dummy" == "dummy");
+  LT_ASSERT_FAIL;
+  LT_ASSERT(!strcmp("dummy" == "dummy");
+  LT_ASSERT_FAULT;
+  LT_ASSERT(!strcmp("dummy" == "dummy");
 
-  RETURN_RESULT;
+  LT_RETURN_RESULT;
 }
