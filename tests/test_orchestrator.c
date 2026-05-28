4e8491fb221f43df1391ff4db46d1db07122dde3
Paul Sinclair <paulsinclair@verizon.net>
2026-05-28 09:35:53 -0700

    Refactor test_orchestrator.c for LiteTest compatibility
    Updated file paths and fixed test function structure.

--- tests/test_orchestrator.c
+++ tests/test_orchestrator.c
@@ -1,37 +1,29 @@
 /**
- * @file /paulsinclair51/litetest/tests/test_orchestrator.c
+ * @file /paulsinclair51/LiteTest/tests/test_orchestrator.c
  
  * @brief Test orchestrtor related functions and macros declared in
-          /paulsinclair51/litetests.h (e.g., TEST, RUN).
+          /paulsinclair51/LiteTest/include/litetests.h (e.g., TESTS, TEST2).
  *
  * @copyright Copyright (c) 2026 paulsinclair51
  * SPDX-License-Identifier: MIT
  * For license details, see @ref ../LICENSE "LICENSE" in the
- * paulsinclair51/lubtype repository root.
+ * paulsinclair51/LiteTest GitHub repository.
  */
 
-#undef TEST_ORCHESTRATOR
-#def TEST_ORCHESTRATOR
+#define TEST_MODULE orchestrator
 #include "litetest.h"
 
-// Test litetest orchestratro related functions.
+// Test LiteTest guard related functions.
 
-TEST(!strcmp("dummy" == "dummy");
+DECLARE_FUNC(orchestrator)
+{ 
+  INIT_TEST;
+ 
+  TEST(!strcmp("dummy" == "dummy");
+  TEST_FAIL;
+  TEST(!strcmp("dummy" == "dummy");
+  TEST_FAULT;
+  TEST(!strcmp("dummy" == "dummy");
 
-if (inject)
-{ ++test_result.injected_fail;
-TEST(0 && "Inject assertion fail 1");
-}
-
-TEST(!strcmp("dummy" == "dummy");
-
-
-if (inject)
-{	++test_result.injected_fail;
-TEST(0 && "Inject assertion fail 2");
-}
-
-TEST(!strcmp("dummy" == "dummy")
-
-return test_result;
+  RETURN_RESULT;
 }
