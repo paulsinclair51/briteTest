2af3788b9f16394bc36dac5d58d8e9920652ee40
Paul Sinclair <16927624+paulsinclair51@users.noreply.github.com>
2026-05-30 17:29:45 +0000

    Align guard test file names and update README/test macro usage

--- tests/README.md
+++ tests/README.md
@@ -9,7 +9,7 @@ For license details, see `../LICENSE`.
 ## `test_litetest.c`
 
 Orchestrator code for running liteTest tests implemented in `test_orchestrator.c`,
-`test_guaad1.c` and `test_guard2.c`.
+`test_guard1.c` and `test_guard2.c`.
 
 ## `test_orchestrator.c`
 
--- tests/test_orchestrator.c
+++ tests/test_orchestrator.c
@@ -1,8 +1,8 @@
 /**
  * @file /paulsinclair51/LiteTest/tests/test_orchestrator.c
  *
- * @brief Test orchestrtor related functions and macros declared in
- *        /paulsinclair51/LiteTest/include/litetests.h (e.g., TESTS, TEST2).
+ * @brief Test orchestrator related functions and macros declared in
+ *        /paulsinclair51/LiteTest/include/litetest.h (e.g., LT_TEST, LT_ASSERT).
  *
  * @copyright Copyright (c) 2026 paulsinclair51
  * SPDX-License-Identifier: MIT
@@ -10,18 +10,17 @@
  * paulsinclair51/LiteTest GitHub repository.
  */
 
-#define TEST_MODULE test_orchestrator
 #include "litetest.h"
 
-DECLARE_FUNC(test_orchestrator)
+LT_DECLARE_FUNCTION(test_orchestrator)
 { 
-  LT_INIT_TEST;
+  LT_INIT_TEST(test_orchestrator, 1);
  
-  LT_ASSERT(!strcmp("dummy" == "dummy");
+  LT_ASSERT(!strcmp("dummy", "dummy"));
   LT_ASSERT_FAIL;
-  LT_ASSERT(!strcmp("dummy" == "dummy");
+  LT_ASSERT(!strcmp("dummy", "dummy"));
   LT_ASSERT_FAULT;
-  LT_ASSERT(!strcmp("dummy" == "dummy");
+  LT_ASSERT(!strcmp("dummy", "dummy"));
 
   LT_RETURN_RESULT;
 }
