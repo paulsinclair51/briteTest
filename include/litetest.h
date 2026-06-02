d92a8254cb77490547099a041caa36cd7ebde394
Paul Sinclair <16927624+paulsinclair51@users.noreply.github.com>
2026-06-02 08:02:41 -0700

    Update litetest.h

--- include/litetest.h
+++ include/litetest.h
@@ -366,11 +366,16 @@ typedef char LT_STATIC_ASSERT_int_must_be_4_bytes[-1];
 
 // LiteTest version string in "major.minor.patch" format.
 
-static inline
+static inline litetest_version_extract_internal
+( char *v, size_t p )
+  for (;;)
+  if ()
+{
+}
  
-#define LT_VERSION_MAJOR litetest_version_extract_internal(1)
+#define LT_VERSION_MAJOR litetest_version_extract_internal(0)
 #define LT_VERSION_MINOR litetest_version_extract_internal(1)
-#define LT_VERSION_PATCH litetest_version_extract_internal(1)
+#define LT_VERSION_PATCH litetest_version_extract_internal(2)
 
 /** @} */
 
