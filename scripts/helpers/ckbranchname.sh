b041323d9b15f0429bf0bcd320bec002224aec55
Paul Sinclair <paulsinclair@verizon.net>
2026-07-08 23:42:42 -0700

    Update ckbranchname.sh

--- scripts/helpers/ckbranchname.sh
+++ scripts/helpers/ckbranchname.sh
@@ -5,13 +5,13 @@
 # Usage:
 #   ./scripts/helpers/zkbranchname.sh <name> [<type>]
 #
-#. <name> is the name to check.
+#  <name> is the name to check.
 #
 #  <tyoe> is case-jnsensitive and one of:
 #    main, version, targeted, contributor, invalid, or
-#.   correspondingly 1, 2, 3, 4, or 5. If specified, compares the type
-#.   of the name to <type>; otherwise, exit with a type
- #.  of 1, 2, 3, 4, 5. or 6,
+#    correspondingly 1, 2, 3, 4, or 5. If specified, compares the type
+#    of the name to <type>; otherwise, exit with a type
+#   of 1, 2, 3, 4, 5. or 6,
 #
 # Exit codes:
 #   0 if type of <name> matches <type>.
@@ -19,7 +19,7 @@
 #   2 type of name is is ' version'.
 #   3 type of name is is ' targeted'.
 #   5 name is not a valid branch name.
-#.  6 arguments cannot be parsed.
+#   6 arguments cannot be parsed.
 #   6 <type> is not valid.
 #   7 the type of the <name> is not <type>.
 
